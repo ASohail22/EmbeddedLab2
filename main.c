19d8b333d86bf63ea571e010105f4c0a5859f720
Aysha Sohail <56938770+ASohail22@users.noreply.github.com>
2022-10-18 23:20:01 -0400

    Q1
    Question 1 for Lab 2

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <msp430.h> 
 
+float a = 10.0;
 
 /**
  * main.c
  */
-int sum(int a, int b);
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	int a = 0b1000000000000000;
-	int b = 0b1000000000000000;
-	int c = sum(a, b);
-	return 0;
-}
 
-int sum(int a, int b)
-{
-    return a + b;
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	unsigned char c = 0x0f;
+	unsigned char d = 0x12;
+	unsigned char andResult;
+	unsigned char orResult;
+
+	int b = 0;
+	b+=1;
+	a = a - 1;
+	andResult = c&d;
+	orResult = c|d;
+	while(1);
 }
